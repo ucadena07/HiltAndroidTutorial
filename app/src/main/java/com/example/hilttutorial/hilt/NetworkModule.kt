84d7921c0001cc2b448c45480c73459d3eeed856
Ulises Cadena <ucadena07@gmail.com>
2024-01-19 06:33:13 -0600

    modules

---
+++ app/src/main/java/com/example/hilttutorial/hilt/NetworkModule.kt
@@ -0,0 +1,11 @@
+package com.example.hilttutorial.hilt
+
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ActivityComponent
+
+@Module
+@InstallIn(ActivityComponent::class)
+class NetworkModule {
+    
+}
